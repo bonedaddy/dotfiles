0de1ae171566be8081f977902493cff807366910
Alex Trottier <postables@rtradetechnologies.com>
2019-11-20 15:09:53 -0800

    bin: add nodejs install/upgrade script

--- bin/install_nodejs.sh
+++ bin/install_nodejs.sh
@@ -2,5 +2,13 @@
 
 # installs nodejs 12 (lts)
 
+UPGRADE="$1"
+
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
+
+if [[ "$UPGRADE" == "--upgrade=yes" ]]; then
+    sudo apt-get install --only-upgrade nodejs
+    sudo npm install -g npm
+else
+    sudo apt-get install -y nodejs
+fi
