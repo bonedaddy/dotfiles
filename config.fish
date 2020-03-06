a070c51bbc9f9c2d794397503d7b17daa5f5d3ed
postables <postables@rtradetechnologies.com>
2020-03-05 23:20:44 -0800

    add fish config

---
+++ config.fish
@@ -0,0 +1,4 @@
+set -x PATH $PATH /usr/local/go/bin /home/solidity/bin /home/solidity/.npm_modules/bin /home/solidity/go/bin /snap/bin
+set -x GOPATH $HOME/go
+set -x GOPRIVATE "github.com/RTradeLtd/*"
+
