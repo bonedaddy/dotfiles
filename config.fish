b9384df860f294ff679f04eac0e7081087271fdf
postables <17089485+bonedaddy@users.noreply.github.com>
2020-03-07 17:23:09 -0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,4 +1,3 @@
-set -x PATH $PATH /usr/local/go/bin /home/solidity/bin /home/solidity/.npm_modules/bin /home/solidity/go/bin /snap/bin
+set -x PATH $PATH /usr/local/go/bin /home/solidity/bin /home/solidity/.npm_modules/bin /home/solidity/go/bin /snap/bin /home/solidity/.local/bin
 set -x GOPATH $HOME/go
 set -x GOPRIVATE "github.com/RTradeLtd/*"
-
