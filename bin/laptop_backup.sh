ed2c79f8d4bcac7844949ee98e7d49642e3538a9
postables <postables@rtradetechnologies.com>
2020-04-14 21:45:41 -0700

    update laptop backup file

--- bin/laptop_backup.sh
+++ bin/laptop_backup.sh
@@ -3,5 +3,13 @@
 # used to backup my laptop to destination directories
 
 BACKUP_DIR="$1"
+ROOT_DIR="/home/$(whoami)"
 
-rsync -avz --exclude "node_module" --exclude ".*" "/home/$(whoami)" "$BACKUP_DIR"
+# general backup that ignores hidden directories and node_modules
+rsync -avz --exclude "node_module" --exclude ".*" "$ROOT_DIR" "$BACKUP_DIR"
+
+# backup important hidden directories and files
+rsync -avz "$ROOT_DIR/.config" "$ROOT_DIR/.ssh" "$ROOT_DIR/.gitconfig" "$ROOT_DIR/.local" "$BACKUP_DIR"
+
+# backup important system files
+rsync -avz "/etc/stubby" /etc/dnsmasq.conf "$BACKUP_DIR"
