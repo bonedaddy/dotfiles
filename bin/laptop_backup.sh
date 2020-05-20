e3d23f428add1611241519d3cc244749fde2e918
postables <postables@rtradetechnologies.com>
2020-05-20 00:14:23 -0700

    update laptop backup script and rpi-utils

--- bin/laptop_backup.sh
+++ bin/laptop_backup.sh
@@ -6,10 +6,10 @@ BACKUP_DIR="$1"
 ROOT_DIR="/home/$(whoami)"
 
 # general backup that ignores hidden directories and node_modules
-rsync -avz --exclude "*node_module*" --exclude ".*" --exclude "Code" "$ROOT_DIR" "$BACKUP_DIR"
+rsync -avz --exclude "*node_module*" --exclude ".*" --exclude "Code" "*go*" "$ROOT_DIR" "$BACKUP_DIR"
 
 # backup important hidden directories and files
-rsync -avz "$ROOT_DIR/.config" "$ROOT_DIR/.ssh" "$ROOT_DIR/.gitconfig" "$ROOT_DIR/.local" "$BACKUP_DIR"
+rsync -avz "$ROOT_DIR/.config" "$ROOT_DIR/.ssh" "$ROOT_DIR/.gitconfig" "$ROOT_DIR/.local" "$ROOT_DIR/.gnupg" "$BACKUP_DIR"
 
 # backup important system files
 rsync -avz "/etc/stubby" /etc/dnsmasq.conf "$BACKUP_DIR"
