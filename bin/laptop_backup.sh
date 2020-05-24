9a49857f3840d8a444ef0053533bd327cb52cdf4
postables <postables@rtradetechnologies.com>
2020-05-24 15:32:53 -0700

    backup metasploit database

--- bin/laptop_backup.sh
+++ bin/laptop_backup.sh
@@ -9,7 +9,7 @@ ROOT_DIR="/home/$(whoami)"
 rsync -avz --exclude "*node_module*" --exclude ".*" --exclude "Code" --exclude "*go*" "$ROOT_DIR" "$BACKUP_DIR"
 
 # backup important hidden directories and files
-rsync -avz "$ROOT_DIR/.config" "$ROOT_DIR/.ssh" "$ROOT_DIR/.gitconfig" "$ROOT_DIR/.gnupg" "$BACKUP_DIR"
+rsync -avz "$ROOT_DIR/.config" "$ROOT_DIR/.ssh" "$ROOT_DIR/.gitconfig" "$ROOT_DIR/.gnupg" "$ROOT_DIR/.msf4" "$BACKUP_DIR"
 
 # backup important system files
 rsync -avz "/etc/stubby" /etc/dnsmasq.conf "$BACKUP_DIR"
