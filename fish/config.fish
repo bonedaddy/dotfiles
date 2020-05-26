30c37ed56c5971285240b56d77791927821bbad2
postables <postables@rtradetechnologies.com>
2020-05-25 21:12:57 -0700

    set anaconda path in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -x PATH $PATH /usr/local/go/bin /home/solidity/bin /home/solidity/.npm_modules/bin /home/solidity/go/bin /snap/bin /home/solidity/.local/bin
+set -x PATH $PATH /usr/local/go/bin /home/solidity/bin /home/solidity/.npm_modules/bin /home/solidity/go/bin /snap/bin /home/solidity/.local/bin /media/solidity/DATA/anaconda/bin/conda
 set -x GOPATH $HOME/go
 set -x GOPRIVATE "github.com/RTradeLtd/*"
 set -x CODE_DIR "/media/solidity/DATA/Code"
