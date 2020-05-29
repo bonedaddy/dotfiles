14fee6510aa7d186657e9309def5da92b755c4fb
postables <postables@rtradetechnologies.com>
2020-05-28 19:15:03 -0700

    update trilium install script

--- install_scripts/trilium_install.sh
+++ install_scripts/trilium_install.sh
@@ -5,17 +5,33 @@
 #       https://github.com/zadam/trilium
 
 # options are server, or desktop
-INSTALL_MODE="desktop"
+INSTALL_MODE="$1"
 INSTALL_VERSION="0.42.3"
 INSTALL_BASE_URL_DESKTOP="https://github.com/zadam/trilium/releases/download/v${INSTALL_VERSION}/trilium_${INSTALL_VERSION}_amd64.deb"
 INSTALL_BASE_URL_SERVER="https://github.com/zadam/trilium/releases/download/v${INSTALL_VERSION}/trilium-linux-x64-server-${INSTALL_VERSION}.tar.xz"
+INSTALL_SERVER_DIR="$HOME/Programs"
+
+if [[ "$INSTALL_MODE" == "" ]]; then
+	INSTALL_MODE="desktop"
+fi
+
+if [[ ! -d "$INSTALL_SERVER_DIR" ]]; then
+	mkdir "$INSTALL_SERVER_DIR"
+fi
+
 
 if [[ "$INSTALL_MODE" == "desktop" ]]; then
         wget -O trilium.deb "$INSTALL_BASE_URL_DESKTOP"
 	sudo dpkg -i trilium.deb
 	rm trilium.deb
 elif [[ "$INSTALL_MODE" == "server" ]]; then
-        echo "server"
+	wget -O trilium-server.tar.xz "$INSTALL_BASE_URL_SERVER"
+	xz -d trilium-server.tar.xz
+	tar xvf trilium-server.tar
+	mv trilium-linux-x64-server  "$INSTALL_SERVER_DIR"
+#	rm -rf trilium-linux-x64-server
+	rm trilium-server.tar
+	rm trilium-server.tar.xz 
 else
         echo "[ERROR] invalid mode, must be desktop or server"
         exit 1
