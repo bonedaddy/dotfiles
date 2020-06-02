8e351047ffeeede19ee1204763cb03ce56335f47
postables <postables@rtradetechnologies.com>
2020-06-01 23:35:42 -0700

    add updated bitwarden backup

--- bin/bitwardenrs_backup.sh
+++ bin/bitwardenrs_backup.sh
@@ -7,4 +7,10 @@ SOURCE_DIR="$1"
 # the destination directory to backup to
 DEST_DIR="$2"
 
+# backup the sqlite data
 sudo sqlite3 "$SOURCE_DIR/db.sqlite3" ".backup $DEST_DIR/bitrsdb_backup.sqlite3"
+
+# backup the attachments folder if exists
+if [[ -d "$SOURCE_DIR/attachments" ]]; then
+    sudo rsync --recursive "$SOURCE_DIR/attachments" "$DEST_DIR"
+fi
