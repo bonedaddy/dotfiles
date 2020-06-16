b6ec1f9ce1e19c742c32259d1f5729a54978c70b
postables <postables@rtradetechnologies.com>
2020-06-16 16:54:00 -0700

    add updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,10 @@
-set -x PATH $PATH /usr/local/go/bin /home/solidity/bin /home/solidity/.npm_modules/bin /home/solidity/go/bin /snap/bin /home/solidity/.local/bin /media/solidity/DATA/anaconda/bin/conda
+set -x PATH $PATH /usr/local/go/bin /home/solidity/bin /home/solidity/.npm_modules/bin /home/solidity/go/bin /snap/bin /home/solidity/.local/bin /media/solidity/DATA/anaconda/bin /home/solidity/.pyenv/bin /usr/local/cuda-11.0/bin
 set -x GOPATH $HOME/go
 set -x GOPRIVATE "github.com/RTradeLtd/*"
 set -x CODE_DIR "/media/solidity/DATA/Code"
 set -x EXT_DIR  "/media/solidity/DATA"
 set -x TRILIUM_DATA_DIR "/media/solidity/DATA/trilium"
+set -x PYENV_ROOT $HOME/.pyenv
+set -x CGO_LDFLAGS "-L/media/solidity/DATA/deepspeech/lib/"
+set -x CGO_CXXFLAGS "-I/media/solidity/DATA/deepspeech/include/"
+set -x LD_LIBRARY_PATH "/media/solidity/DATA/deepspeech/lib/:/usr/lib/cuda/lib64/:LD_LIBRARY_PATH"
